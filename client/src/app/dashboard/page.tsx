b6adcf74875da8035e370b6a90d15a8a5180234a
Josh Hashimoto <hashimotojosh@gmail.com>
2025-06-20 03:06:01 -0700

    changed dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { useEffect } from 'react';
-import { useGetUploadsQuery } from '../../redux/api';
 import PdfList from '../../components/PdfList';
 import { useRouter } from 'next/navigation';
 
 export default function DashboardPage() {
-  const { data, error, isLoading } = useGetUploadsQuery();
   const router = useRouter();
 
   useEffect(() => {
@@ -16,9 +14,7 @@ export default function DashboardPage() {
   return (
     <main style={{ padding: 40 }}>
       <h1>Uploaded PDFs</h1>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error loading uploads.</p>}
-      {data && <PdfList uploads={data} />}
+      <PdfList />
     </main>
   );
 }
